d84a7c5d5da9d1a06508b7ca58febd7ab6e8418a
spacemonkeywest <caldaco12@gmail.com>
2024-08-13 11:07:41 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<SFML/Graphics.hpp>
+
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(600, 800), "Breakout");
+
+	while (window.isOpen()) {
+		sf::Event event;
+
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
+}
