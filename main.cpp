d75d23054bbaa5ec9503082ef7ac816c66dbabce
spacemonkeywest <caldaco12@gmail.com>
2024-08-13 11:19:28 -0700

    added paddle and bounds checking

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 #include<SFML/Graphics.hpp>
 
-
 int main() {
 	sf::RenderWindow window(sf::VideoMode(600, 800), "Breakout");
+
+	sf::RectangleShape paddle;
+	paddle.setSize(sf::Vector2f(80, 10));
+	paddle.setFillColor(sf::Color::White);
+	paddle.setOutlineColor(sf::Color::White);
+	paddle.setPosition(260, 750);
+
+	float moveSpeed = 0.1f;
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -14,7 +21,22 @@ int main() {
 			}
 		}
 
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+			float newX = paddle.getPosition().x;
+			if (newX > 0) {
+				paddle.move(-moveSpeed, 0);
+			}
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+			float newX = paddle.getPosition().x;
+			if (newX < 520) {
+				paddle.move(moveSpeed, 0);
+			}
+		}
+
 		window.clear();
+		window.draw(paddle);
 		window.display();
 	}
 
