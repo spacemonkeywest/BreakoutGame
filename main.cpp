7a4ac55d9230b9d601301419c3ab563f127fbc26
spacemonkeywest <caldaco12@gmail.com>
2024-08-13 15:01:02 -0700

    turned main into Game class. Made ball reset if it gets past paddle

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<SFML/Graphics.hpp>
+#include<vector>
 
 class Brick {
 	private:
@@ -26,84 +27,117 @@ class Brick {
 			broken = true;
 		}
 
-		sf::RectangleShape getShape() {
+		sf::RectangleShape& getShape() {
 			return brick;
 		}
 };
 
-int main() {
-	sf::RenderWindow window(sf::VideoMode(600, 800), "Breakout");
+class Game {
+	private:
+		sf::RenderWindow window;
+		sf::RectangleShape paddle;
+		sf::CircleShape ball;
+		std::vector<Brick> bricks;
+		sf::Vector2f velocity;
+		float moveSpeed;
 
-	sf::RectangleShape paddle;
-	paddle.setSize(sf::Vector2f(80, 10));
-	paddle.setFillColor(sf::Color::White);
-	paddle.setOutlineColor(sf::Color::White);
-	paddle.setPosition(260, 750);
+		void initWindow() {
+			window.create(sf::VideoMode(600, 800), "Breakout");
+		}
 
-	sf::CircleShape ball;
-	ball.setRadius(10);
-	ball.setFillColor(sf::Color::White);
-	ball.setOutlineColor(sf::Color::White);
-	ball.setPosition(295, 395);
+		void initPaddle() {
+			paddle.setSize(sf::Vector2f(80, 10));
+			paddle.setFillColor(sf::Color::White);
+			paddle.setPosition(260, 750);
+		}
 
-	float moveSpeed = 0.125f;
-	sf::Vector2f velocity(0.075f, 0.075f);
+		void initBall() {
+			ball.setRadius(10);
+			ball.setFillColor(sf::Color::White);
+			ball.setPosition(295, 395);
+		}
 
-	std::vector<Brick> bricks;
-	bricks.push_back(Brick(20, 20, 0, 0));
-	bricks.push_back(Brick(20, 20, 25, 0));
-	bricks.push_back(Brick(20, 20, 50, 0));
-	bricks.push_back(Brick(20, 20, 75, 0));
+		void initBricks() {
+			bricks.push_back(Brick(20, 20, 0, 0));
+			bricks.push_back(Brick(20, 20, 25, 0));
+			bricks.push_back(Brick(20, 20, 50, 0));
+			bricks.push_back(Brick(20, 20, 75, 0));
+		}
 
-
-	while (window.isOpen()) {
-		sf::Event event;
-
-		while (window.pollEvent(event)) {
-			if (event.type == sf::Event::Closed) {
-				window.close();
+		void processEvents() {
+			sf::Event event;
+			while (window.pollEvent(event)) {
+				if (event.type == sf::Event::Closed) {
+					window.close();
+				}
 			}
 		}
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-			float newX = paddle.getPosition().x;
-			if (newX > 0) {
-				paddle.move(-moveSpeed, 0);
+		void update() {
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+				if (paddle.getPosition().x > 0) {
+					paddle.move(-moveSpeed, 0);
+				}
 			}
-		}
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-			float newX = paddle.getPosition().x;
-			if (newX < 520) {
-				paddle.move(moveSpeed, 0);
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+				if (paddle.getPosition().x < 520) {
+					paddle.move(moveSpeed, 0);
+				}
 			}
-		}
 
-		if (ball.getPosition().y < 0 || ball.getGlobalBounds().intersects(paddle.getGlobalBounds())) {
-			velocity.y = -velocity.y;
-		} else if (ball.getPosition().x < 0 || ball.getPosition().x > 580) {
-			velocity.x = -velocity.x;
-		}
-
-		for (auto& brick: bricks) {
-			if (ball.getGlobalBounds().intersects(brick.getShape().getGlobalBounds())) {
+			if (ball.getPosition().y < 0 || ball.getGlobalBounds().intersects(paddle.getGlobalBounds())) {
 				velocity.y = -velocity.y;
-				brick.setBroken();
+			} else if (ball.getPosition().x < 0 || ball.getPosition().x > 580) {
+				velocity.x = -velocity.x;
 			}
+
+			if (ball.getPosition().y > 780) {
+				ball.setPosition(295, 395);
+			}
+
+			for (auto& brick : bricks) {
+				if (!brick.isBroken() && ball.getGlobalBounds().intersects(brick.getShape().getGlobalBounds())) {
+					velocity.y = -velocity.y;
+					brick.setBroken();
+				}
+			}
+
+			ball.move(velocity);
 		}
 
-		ball.move(velocity);
+		void render() {
+			window.clear();
+			window.draw(paddle);
+			window.draw(ball);
+			for (auto& brick : bricks) {
+				if (!brick.isBroken()) {
+					window.draw(brick.getShape());
+				}
+			}
+			window.display();
+		}
 
-		window.clear();
-		window.draw(paddle);
-		window.draw(ball);
-		for (auto& brick: bricks) {
-			if (!brick.isBroken()) {
-				window.draw(brick.getShape());
+	public:
+		Game() : moveSpeed(0.125f), velocity(0.075f, 0.075f) {
+			initWindow();
+			initPaddle();
+			initBall();
+			initBricks();
+		}
+
+		void run() {
+			while (window.isOpen()) {
+				processEvents();
+				update();
+				render();
 			}
 		}
-		window.display();
-	}
+};
+
+int main() {
+	Game game;
+	game.run();
 
 	return EXIT_SUCCESS;
 }
